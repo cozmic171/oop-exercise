9f5a7e5339aa0be6887f9ad455ee7b377618178c
Ben Graham <ben.graham@igroupaustralia.com.au>
2016-05-06 18:14:06 +1000

    Initial Commit

---
+++ oop exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oop exercise
+//
+//  Created by Ben Graham on 6/05/2016.
+//  Copyright © 2016 Ben Graham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
